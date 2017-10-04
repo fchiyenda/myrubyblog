2b1790f193cbeb76354f036413052a90d7c772d9
Fredrick Chiyenda <fchiyenda@gmail.com>
2017-10-05 00:08:16 +0200

    Adde CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,42 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @post = Post.all
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
-  def destroy
+  def new
+    @post = Post.new
+    @category = Category.all
+  end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:title,:body,:category_id))
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params.require(:id))
+    if @post.update_attributes(params.require(:post).permit(:title,:body,:category_id))
+      redirect_to post_path, :notice => "Your post has been updated"
+    else
+        render "edit"
+    end 
+  end 
+
+  def destroy
+    @post = Post.find(params.require(:id))
+    @post.destroy!
+    redirect_to posts_path, :notice => "Your post has been deleted"
   end
 end
