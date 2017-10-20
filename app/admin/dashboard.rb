8490f9606fc7caada7bbea903fdd4f31e02f6cf6
Fredrick Chiyenda <fchiyenda@gmail.com>
2017-10-20 23:25:52 +0200

    Completed Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,13 +1,28 @@
-ActiveAdmin::Dashboards.build do
-    section "Recent Posts" do
-      table_for Post.order("id desc").limit(15) do
-        column :id
-        column "Post Title",:title do |post|
-          link_to post.title,[:admin,post]
+ActiveAdmin.register_page "Dashboard" do
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Recent Posts" do
+          table_for Post.order("id desc").limit(5) do
+            column :name do |post|
+              link_to post.title, [:admin, post]
+            end
+            column :created_at
+          end
+          strong (link_to "Show All Posts" , :admin_posts )
         end
-        column :category,:sortable => :category
-        column :created_at
       end
-      strong (link_to "Show All Posts",:posts)
+      column do
+        panel "Categories" do
+          table_for Category.order("id desc").limit(5) do
+            column :name do |category|
+              link_to category.name, [:admin, category]
+            end
+            column :created_at
+          end
+          strong (link_to "Show All Categories" , :admin_categories )
+        end
+      end
     end
+  end
 end
